aa5ac06e55053935a243f58c16b4b12c735c59b7
徐坤坤 <4142944942@qq.com>
2021-03-05 12:02:32 +0800

    # update favor list

--- flutter_helloworld/lib/learningpart/favorList.dart
+++ flutter_helloworld/lib/learningpart/favorList.dart
@@ -10,12 +10,47 @@ class RandomWords extends StatefulWidget {
   _RandomWordsState createState() => _RandomWordsState();
 }
 
+class SavedWords extends StatelessWidget {
+  final List<WordPair> items;
+  const SavedWords(this.items, {Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(
+          'Saved Words',
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+      body: _buildItems(),
+    );
+  }
+
+  Widget _buildItems() {
+    return new ListView.separated(
+      itemCount: items.length,
+      separatorBuilder: (BuildContext context, int index) {
+        return new Divider();
+      },
+      itemBuilder: (BuildContext context, int index) {
+        return new ListTile(
+          title: new Text(
+            items[index].asPascalCase,
+            style: const TextStyle(fontSize: 18, color: Colors.black),
+          ),
+        );
+      },
+    );
+  }
+}
+
 class _RandomWordsState extends State<RandomWords> {
   List<WordPair> _items = <WordPair>[];
+  Set _saveItems = new Set<WordPair>();
   TextStyle _biggerFont = const TextStyle(fontSize: 18, color: Colors.black);
 
   void initState() {
-    _items.addAll(generateWordPairs().take(10));
+    _items.addAll(generateWordPairs().take(30));
     super.initState();
   }
 
@@ -23,10 +58,28 @@ class _RandomWordsState extends State<RandomWords> {
     print("item length = ${_items.length}");
     return new Scaffold(
       appBar: new AppBar(
-        title: Text('Random Words Demo'),
+        title: Text(
+          'Random Words Demo',
+          style: const TextStyle(color: Colors.white),
+        ),
+        actions: <Widget>[
+          new IconButton(
+              icon: const Icon(
+                Icons.list,
+                color: Colors.white,
+              ),
+              onPressed: _onPushPressed),
+        ],
       ),
       body: _buildItems(),
     );
+  }
+
+  void _onPushPressed() {
+    print('Push Pressed!');
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return SavedWords(_saveItems.toList());
+    }));
   }
 
   Widget _buildItems() {
@@ -37,21 +90,28 @@ class _RandomWordsState extends State<RandomWords> {
         return new Divider();
       },
       itemBuilder: (BuildContext context, int index) {
-        if (index >= _items.length - 1) {
-          _items.addAll(generateWordPairs().take(10));
-
-          // setState(() {});
-        }
-        print("item index = ${index}, item length = ${_items.length}");
-
         return _buildRow(_items[index]);
       },
     );
   }
 
   Widget _buildRow(WordPair pair) {
+    bool _isSave = _saveItems.contains(pair);
     return new ListTile(
       title: new Text(pair.asPascalCase, style: _biggerFont),
+      trailing: new Icon(
+        _isSave ? Icons.favorite : Icons.favorite_border,
+        color: _isSave ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (_isSave) {
+            _saveItems.remove(pair);
+          } else {
+            _saveItems.add(pair);
+          }
+        });
+      },
     );
   }
 }
@@ -63,6 +123,9 @@ class RandomListApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Random Words Demo',
+      theme: new ThemeData(
+        primaryColor: Colors.lightBlue,
+      ),
       home: new RandomWords(),
     );
   }
